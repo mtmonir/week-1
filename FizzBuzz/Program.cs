32b360ff21480e9956581d18a083df99d8ccb92c
tahir <tahir.monir@gmail.com>
2021-03-03 16:17:46 -0600

    initial commit...

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz // scope, prevents name collusion
+{
+    //class (reference type), struct (value type), interface (reference type), enum (value type)
+    internal class StartingPoint
+    {
+        private int startPoit  = 0;
+        private int endPoint = 0;
+
+        public static void userInput()
+        { 
+            System.Console.WriteLine("Please enter two numbers:");
+            
+
+        }
+        public static void Main(string[] args)
+        {
+            //if divisible by 3 print Fizz
+            //if divisible by 5 print Buzz
+            //if divisible by both print FizzBuzz
+            
+            // get user input
+            var value = 0;
+            const string fizz = "Fizz";
+            const string buzz = "Buzz";
+
+            var firstCheck = 3;
+            var secondCheck = 5;
+
+            for (;value < 50; value += 1)
+            {
+                if (value % firstCheck == 0)
+                {
+                    Console.Write(fizz);
+                }
+
+                if (value % secondCheck == 0)
+                {
+                    System.Console.WriteLine(buzz);
+                }
+            }
+            
+        }
+
+    }
+}
